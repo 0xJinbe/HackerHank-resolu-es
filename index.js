7eda6e90bf71f33fc6836a3421f1f156483cb42e
Claudio W. Germano Jr <>
2021-05-03 16:29:38 +0000

    Desafio 7,8,9

--- index.js
+++ index.js
@@ -1,91 +1,69 @@
-// maiores de idade
-
-function solucao(idade) {
-  if(idade >= 18) {
-      console.log('PODE ENTRAR')
-  } else {
-      console.log('ACESSO NEGADO')
-  }
-  
-}
-
-solucao(15)
-
-// verificar modo do retrato foto
-
-function solucao(largura, altura) {
-  if (altura > largura) {
-      console.log('RETRATO')
-  } else if (largura > altura) {
-      console.log('PAISAGEM')
-  } else if ( largura === altura) {
-      console.log('QUADRADA')
-  }
-  
-}
-
-solucao(8, 8)
-
-// pedra papel tesoura
-
-
-function solucao(joao, andre) {
-    if(joao == 'PEDRA' && andre == 'TESOURA') {
-        console.log('JOAO')
-    } else if (joao == 'PEDRA' && andre == 'PAPEL') {
-        console.log('ANDRE')
-    } else if (joao == 'PAPEL' && andre == 'PEDRA') {
-        console.log('JOAO')
-    } else if (joao == 'PAPEL' && andre == 'TESOURA') {
-        console.log('ANDRE')
-    } else if (joao == 'TESOURA' && andre == 'PAPEL') {
-        console.log('JOAO')
-    } else if (joao == 'TESOURA' && andre == 'PEDRA') {
-        console.log('ANDRE')
-    } else if (joao == 'TESOURA' && andre == 'TESOURA'){
-        console.log('EMPATE')
-    } else if (joao == 'PEDRA' && andre == 'PEDRA') {
-        console.log('EMPATE')
-    } else if (joao == 'PAPEL' && andre == 'PAPEL') {
-        console.log('EMPATE')
-    }
-}
-solucao('PEDRA', 'TESOURA')
-
-// CONTAGEM REGRESSIVA
-
-function solucao(numero) {
-  while (numero > -1) {
-    console.log(numero);
-    numero--
-    }
-console.log('KABUM')
-}
-solucao(10)
-
-// condicionais para cinema
-let obj = {
-  temIngresso: true,
-  idade: 17,
-  temCarteirinha: true,
-  censura: 16,
-  estaComPais: false
-};
-
-function solucao(obj) {
-	if (obj.temIngresso == true && obj.idade >= obj.censura || obj.estaComPais == true) {       
-       if (obj.temCarteirinha == true || obj.idade < 18) {
-      console.log('MEIA')
-    } else (obj.temCarteirinha == false || obj.idade > 18) {
-      console.log('INTEIRA')
-     }
-    
-    } else (obj.temIngresso == false && obj.idade < obj.censura || obj.estaComPais == false ) {
-        console.log('ACESSO NEGADO')
-      }
-} 
-
-solucao(obj)
-
 //Mobileapp
 
+let t = {
+  tempo: 2,
+  distancia: 1
+}
+
+function solucao(tempo, distancia) {
+  let corrida = 0
+  if (tempo < 5) {
+    corrida = 600
+  } else if (5 < tempo && tempo < 60 ) {
+    corrida = (100 * tempo) + (50 * distancia)
+  } else if (tempo >= 60) {
+    if (distancia < 100) {
+      corrida = 200 * distancia
+    } else {
+      corrida = 150 * distancia
+    }
+  } console.log(corrida)
+}
+
+solucao(25,8)
+
+//ano olimpico
+
+
+function solucao(ano) {
+  const resultado = ano % 4;
+  
+  if (ano % 4 == 2) {
+    console.log('COPA')
+  } else if (ano % 4 == 0) {
+    console.log('JOGOS')
+  } else if (ano % 4 == 1) {
+    console.log('MEH')
+  } 
+  
+}
+solucao(2019)
+
+// filtrar letras
+
+let lista = 
+[
+  {
+    "letra": "m",
+    "palavras": [
+      "mamao",
+      "maca",
+      "melao",
+      "melancia",
+      "laranja"
+    ]
+  }
+]
+
+
+function solucao(letra, palavras) {
+  let contador = 0;
+
+  for (let item of palavras) {
+    if (item[0] != letra ) {
+      contador ++;
+  } else {
+    continue
+    }
+} console.log(contador)
+}
